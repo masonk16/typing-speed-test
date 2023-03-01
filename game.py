73375d41e196174f363234598d982d2e54072bea
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-03-01 21:43:50 +0200

    Correct path for reset icon image

--- game.py
+++ game.py
@@ -25,14 +25,14 @@ class Game:
         self.RESULT_C = (255, 70, 70)
 
         pygame.init()
-        self.open_img = pygame.image.load('type-speed-open.png')
+        self.open_img = pygame.image.load('img/type-speed-open.png')
         self.open_img = pygame.transform.scale(self.open_img, (self.width, self.height))
 
         self.bg = pygame.image.load('img/background.jpg')
         self.bg = pygame.transform.scale(self.bg, (500, 750))
 
         self.screen = pygame.display.set_mode((self.width, self.height))
-        pygame.display.set_caption('Type Speed test')
+        pygame.display.set_caption('Typing Speed Test')
 
     def draw_text(self, screen, msg, y, f_size, color):
         font = pygame.font.Font(None, f_size)
